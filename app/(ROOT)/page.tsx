c1e35ba001587f9b196e8c808d5f9475dc6e570e
moe <gidoonotful2002@gmail.com>
2025-08-28 15:49:39 +0300

    added shadcn

--- app/(ROOT)/page.tsx
+++ app/(ROOT)/page.tsx
@@ -1,9 +1,14 @@
-import Image from "next/image";
+import SearchForm from "../components/SearchForm";
+
 
 export default function Home() {
   return (
     <>
-      HOME
+      <section className="w-full bg-primary1 min-h-[530px] pattern flex justify-center items-center flex-col py-10 px-6">
+        <h1 className="heading">Pitch your Startup, <br/> Connect with entrepreneurs</h1>
+        <p className="font-medium text-[20px] text-white max-w-3xl text-center break-words">Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions. </p>
+        <SearchForm/>
+      </section> 
     </>
   );
 }
